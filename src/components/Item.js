80a92effe80c4db95a747cb062c65e33e3f51392
rovenelabanga <rovenelabanga2001@gmail.com>
2024-11-04 12:56:34 +0300

    uses state to manage variables that change dynamically

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function Item({ name, category }) {
+  const [isActive, setIsActive] = React.useState(false);
+
+  function handleToggle() {
+    setIsActive((prevState) => !prevState);
+  }
   return (
-    <li className="">
+    <li className={isActive ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleToggle}>
+        {isActive ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,10 +2,23 @@ import React from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setCategory] = React.useState("All");
+
+  function handleFilterChange(event) {
+    setCategory(event.target.value);
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if ((selectedCategory === "All")) {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  });
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +26,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
